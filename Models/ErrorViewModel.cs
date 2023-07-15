6830c1d08a52ab33fbe4797fc2d142c39f0e2c70
Nhi Nguyen <nhing17899@gmail.com>
2023-07-15 18:14:48 -0400

    add new project MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_version_control.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
